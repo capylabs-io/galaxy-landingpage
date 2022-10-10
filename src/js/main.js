be584e4ab91d270625ffb32c0056cc7be65c8627
t-linh <linhtrinh.a8@gmail.com>
2022-10-10 19:47:32 +0900

    fix block 2

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,7 @@ const fullPageObject = document.getElementsByClassName('fullpage')
 const triggerBottom = window.innerHeight / 5 * 4;
 var timer
 var countTrigger = 0
+var isUserScrolling = false
 
 window.onload = (event) => {
     const fullPageObjectArr = Array.from(fullPageObject)
@@ -19,6 +20,7 @@ window.onload = (event) => {
     fullPageObjectArr[3].addEventListener('scroll', function(event) {
         callDisplayTransaction()
     })
+    handleInBlock3(fullPageObjectArr)
 };
 window.addEventListener('scroll', function(event) {
     callDisplayTransaction()
@@ -66,59 +68,76 @@ function handleInBlock1(fullPage) {
 }
 
 function handleInBlock2(fullPage) {
-    fullPage[1].addEventListener('wheel', function(e) {
-        preventScroll(e)
+    fullPage[1].addEventListener("wheel", function (e) {
+        preventScroll(e);
         // block 1 - image hidden
-        let block1Img =  Array.from(document.getElementsByClassName('bl1-img'))
-            block1Img.forEach(item => {
-            item.classList.remove('show')
-        })
-        if (e.deltaY > 0) {
-            countTrigger++
-        } else if (e.deltaY < 0) {
-            countTrigger--
+        let block1Img = Array.from(document.getElementsByClassName("bl1-img"));
+        block1Img.forEach((item) => {
+          item.classList.remove("show");
+        });
+        if (e.deltaY > 0 && !isUserScrolling) {
+          isUserScrolling = true;
+          countTrigger++;
+          setTimeout(function () {
+            isUserScrolling = false;
+          }, 500);
+        } else if (e.deltaY < 0 && !isUserScrolling) {
+          isUserScrolling = true;
+          countTrigger--;
+          setTimeout(function () {
+            isUserScrolling = false;
+          }, 500);
         }
-        console.log(countTrigger)
+    
         if (countTrigger < 0) {
-            countTrigger = undefined
-            fullPage[0].scrollIntoView()
-            callDisplayTransaction()
-        } else if (countTrigger <= 7) {
-            Array.from(showingTrigger).forEach(trigger => {
-                trigger.classList.remove('show')
-            })
-            Array.from(trgger1Class).forEach(trigger => {
-                trigger.classList.add('show')
-            })
-        } else if (countTrigger <= 14) {
-            Array.from(showingTrigger).forEach(trigger => {
-                trigger.classList.remove('show')
-            })
-            Array.from(trgger2Class).forEach(trigger => {
-                trigger.classList.add('show')
-            })
-        } else if (countTrigger <= 21) {
-            Array.from(showingTrigger).forEach(trigger => {
-                trigger.classList.remove('show')
-            })
-            Array.from(trgger3Class).forEach(trigger => {
-                trigger.classList.add('show')
-            })
-        } else if (countTrigger > 18) {
-            countTrigger = undefined
-            var triggerBottom = window.innerHeight / 5 * 4;
-            fullPage[2].scrollIntoView()
-            Array.from(block3Item).forEach(item => {
-                if (!item.classList.contains('process-active')) {
-                    if (item.getBoundingClientRect().top < triggerBottom) {
-                        item.classList.add('process-active')
-                        loadingBlock3(0)
-                    }
-                }
-            })
+          countTrigger = -1;
+          fullPage[0].scrollIntoView();
+          callDisplayTransaction();
+        } else if (countTrigger == 0) {
+          Array.from(showingTrigger).forEach((trigger) => {
+            trigger.classList.remove("show");
+          });
+          Array.from(trgger1Class).forEach((trigger) => {
+            trigger.classList.add("show");
+          });
+        } else if (countTrigger == 1) {
+          Array.from(showingTrigger).forEach((trigger) => {
+            trigger.classList.remove("show");
+          });
+          Array.from(trgger2Class).forEach((trigger) => {
+            trigger.classList.add("show");
+          });
+        } else if (countTrigger == 2) {
+          Array.from(showingTrigger).forEach((trigger) => {
+            trigger.classList.remove("show");
+          });
+          Array.from(trgger3Class).forEach((trigger) => {
+            trigger.classList.add("show");
+          });
+        } else if (countTrigger > 2) {
+          countTrigger = 3;
+          var triggerBottom = (window.innerHeight / 5) * 4;
+          fullPage[2].scrollIntoView();
+          Array.from(block3Item).forEach((item) => {
+            if (!item.classList.contains("process-active")) {
+              if (item.getBoundingClientRect().top < triggerBottom) {
+                item.classList.add("process-active");
+                loadingBlock3(0);
+              }
+            }
+          });
         }
-    })
+      });
+  }
 
+function handleInBlock3(fullPage) {
+  fullPage[2].addEventListener("wheel", function (event) {
+    if (event.deltaY < 0) {
+        if (fullPage[2].getBoundingClientRect().top <= 20) {
+          fullPage[1].scrollIntoView();
+        }
+      }
+  });
 }
 
 function preventScroll(e){
